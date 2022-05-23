9445c2ee776a728d8ec226a7cf77a9e4a7c81500
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-05-23 16:31:20 +0430

    create Logout view

--- Backend/Account/urls.py
+++ Backend/Account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     Login,
+    Logout,
 )
 
 
 app_name = 'account'
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
--- Backend/Account/views.py
+++ Backend/Account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import (
     views as auth_views,
     update_session_auth_hash
@@ -11,3 +12,7 @@ from .forms import (
 class Login(auth_views.LoginView):
     form_class = LoginForm
     template_name = 'Account/login.html'
+
+
+class Logout(LoginRequiredMixin, auth_views.LogoutView):
+    pass
