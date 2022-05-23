2278331e7b3ac2a01aecfaf75b915b6dc6df969e
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-05-23 16:21:38 +0430

    create Login django-form

--- Backend/Account/forms.py
+++ Backend/Account/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from django.contrib.auth import get_user_model, authenticate
+from django.core.exceptions import ValidationError
+from django.utils.text import capfirst
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    PasswordResetForm
+)
 import re
+
+User = get_user_model()
 
 
 def validate_email(email):
@@ -20,3 +30,31 @@ def validate_phone(number):
     if k == 1:
         return True
     return False
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'email', 'id': 'email', 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'password', 'id': 'password', 'class': 'form-control'})
+    )
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        if username is not None and password:
+            self.user_cache = User.objects.filter(username=username).first()
+            if not self.user_cache:
+                self.user_cache = User.objects.filter(email=username).first()
+            if self.user_cache is None:
+                self.add_error('username', 'ایمیل یا نام کاربری اشتباه است!')
+            else:
+                self.confirm_login_allowed(self.user_cache)
+                if self.user_cache.check_password(password):
+                    return self.cleaned_data
+                else:
+                    self.add_error('password', 'رمز وارد شده اشتباه است!')
+                    if not self.user_cache.is_active:
+                        self.add_error('password', 'حساب کاربری شما فعال نیست!')
+        return self.cleaned_data
