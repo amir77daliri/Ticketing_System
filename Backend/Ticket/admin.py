ab751047d89ae6df0d1214b8a8d264c1ecae1caa
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-06-05 14:18:38 +0430

    create Ticket model and add to django-admin

--- Backend/Ticket/admin.py
+++ Backend/Ticket/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_content', 'tracking_code', 'status', 'is_open']
+    list_filter = ['created_at', 'status']
+    search_fields = ('tracking_code', 'title')
+
+    def sub_content(self, obj):
+        return f"{obj.content[:30]}"
+    sub_content.short_description = 'content'
--- Backend/Ticket/models.py
+++ Backend/Ticket/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from extensions.utils import jalali_converter
+import random
 
-# Create your models here.
+
+class Ticket(models.Model):
+    STATUS_CHOICES = (
+        ('P', 'در حال بررسی'),  # pending
+        ('A', 'پاسخ داده شده'),  # Answered
+    )
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    tracking_code = models.IntegerField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_open = models.BooleanField(default=True)
+    # related fields :
+    user = models.ForeignKey(get_user_model(), related_name='tickets', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created_at', 'id']
+
+    def __str__(self):
+        return f"{self.title}/{self.tracking_code}"
+
+    def jpublish(self):
+        return jalali_converter(self.created_at)
+
+    def save(self, *args, **kwargs):
+        while True:
+            if not self.tracking_code:
+                try:
+                    tracking_code = random.randint(1000, 99999)
+                    self.tracking_code = tracking_code
+                    self.slug = slugify(f'ticket {self.tracking_code}')
+                    super(Ticket, self).save(*args, **kwargs)
+                    break
+                except:
+                    pass
+            else:
+                self.slug = slugify(f'ticket {self.tracking_code}')
+                super(Ticket, self).save(*args, **kwargs)
+                break
