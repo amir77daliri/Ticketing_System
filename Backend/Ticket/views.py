ecaf762ba798f477c2808e4624fea534ad1a888e
elaahe-sh <elaheshoja17@gmail.com>
2022-06-06 22:11:09 +0430

    admin-dashboard view, url and templates,, seprate aside, modal and footer html codes

--- Backend/Ticket/views.py
+++ Backend/Ticket/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import AdminAccessMixin
 
-# Create your views here.
+
+class AdminProfile(LoginRequiredMixin, AdminAccessMixin, ListView):
+    queryset = Ticket.objects.all()
+    template_name = 'Ticket/admin-dashboard.html'
+    context_object_name = 'tickets'
+    paginate_by = 2
+
