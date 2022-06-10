d1c041bd1ad9b9895fe2479d32a02adb7163aa96
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-06-10 20:55:10 +0430

    create profile view and change password view,, config templates and urls

--- Backend/Account/urls.py
+++ Backend/Account/urls.py
@@ -6,16 +6,19 @@ from .views import (
     activate,
     MyPasswordReset,
     MyPasswordResetDone,
-    MyPasswordResetConfirm
+    MyPasswordResetConfirm,
+    ProfileUpdate,
+    change_password
 )
-
 
 app_name = 'account'
 urlpatterns = [
+    path('info/', ProfileUpdate.as_view(), name='profile-info'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
     path('register/', Register.as_view(), name='register'),
     path('active/<uidb64>/<token>/', activate, name='activate'),
+    path('password_change/', change_password, name='password_change'),
     path('password_reset/', MyPasswordReset.as_view(), name='password_reset'),
     path('password_reset/done/', MyPasswordResetDone.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', MyPasswordResetConfirm.as_view(), name='password_reset_confirm'),
--- Backend/Account/views.py
+++ Backend/Account/views.py
@@ -1,37 +1,39 @@
-from django.shortcuts import render, HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
-from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
-from .tokens import account_activation_token
-from django.urls import reverse_lazy
-from django.contrib.auth import get_user_model
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from django.contrib.auth import (
     views as auth_views,
     update_session_auth_hash
 )
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from .tokens import account_activation_token
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import (
     LoginForm,
     SignUpForm,
     MyPasswordResetForm,
-    SetNewResetPasswordForm
+    SetNewResetPasswordForm,
+    ChangePasswordForm,
+    ProfileUpdateForm
 )
 
 
 User = get_user_model()
 
 
+def home(request):
+    return render(request, 'Account/index.html', {})
+
+
 class Login(auth_views.LoginView):
     form_class = LoginForm
     template_name = 'Account/login.html'
-    success_url = reverse_lazy('home')
-
-
-def home(request):
-    return render(request, 'Account/index.html', {})
 
 
 class Logout(LoginRequiredMixin, auth_views.LogoutView):
@@ -46,20 +48,22 @@ class Register(CreateView):
         user = form.save(commit=False)
         user.is_active = False
         user.save()
+        use_https = self.request.is_secure()
         current_site = get_current_site(self.request)
         mail_subject = 'فعالسازی حساب کاربری'
         message = render_to_string('Account/active_account_email.html', {
             'user': user,
             'domain': current_site.domain,
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': account_activation_token.make_token(user)
+            'token': account_activation_token.make_token(user),
+            "protocol": "https" if use_https else "http"
         })
         to_email = form.cleaned_data.get('email')
         email = EmailMessage(
             mail_subject, message, to=[to_email]
         )
         email.send()
-        return render(self.request, 'Account/email-sent.html', {})
+        return render(self.request, 'Account/email-sent.html', {'email': form.cleaned_data.get('email')})
 
 
 def activate(request, uidb64, token):
@@ -90,5 +94,44 @@ class MyPasswordResetDone(auth_views.PasswordResetDoneView):
 
 class MyPasswordResetConfirm(auth_views.PasswordResetConfirmView):
     form_class = SetNewResetPasswordForm
-    template_name = 'Account/password_reset_confirm.html'
+    template_name = 'Account/password-reset-confirm.html'
     success_url = reverse_lazy('account:login')
+
+
+@login_required()
+def change_password(request):
+    form = ChangePasswordForm(request.user, request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('ticket:user-profile')
+    context = {
+        'form': form
+    }
+    return render(request, 'Account/change-password.html', context)
+
+
+class ProfileUpdate(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileUpdateForm
+    template_name = 'Account/profile.html'
+    success_url = reverse_lazy('account:profile-info')
+
+    def get_object(self, **kwargs):
+        user = get_object_or_404(User, pk=self.request.user.id)
+        return user
+
+    def get_form_kwargs(self):
+        kwargs = super(ProfileUpdate, self).get_form_kwargs()
+        kwargs.update(
+            {'user': self.request.user}
+        )
+        return kwargs
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        print(form.cleaned_data.get('profile_image'))
+        self.object = form.save()
+        return super().form_valid(form)
+
